6c989919f4f35b325e90db879d7b51c263b4ca42
ArtVelych <artemvelychko1999@gmail.com>
2025-04-18 19:01:28 +0200

    change App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
         price={14.29}
       />
-      <Product name="Quesadilla" price={9.75} />
+      <Product name="Quesadilla + Szaverma!!!" price={9.75} />
     </div>
   );
 }
